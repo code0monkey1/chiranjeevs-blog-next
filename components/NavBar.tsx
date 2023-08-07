cf7168f8ab73c6b469e359ef4f99ab0a4731aa39
Chiranjeev Thomas <chiranjeevwork@gmail.com>
2023-08-07 04:16:05 +0000

    7 Aug 2023, 09:46

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -31,9 +31,9 @@ const NavBar = () => {
             height={90}
             className="md:ml-0 h-20 w-20 mt-6 md:h-32 md:w-32 md:mt-0"
           />
-          <span className=" font-bold ml-2 text-primary text-5xl mt-7 ml-2 font-extrabold pb-4 ">
+          <span className=" text-primary text-5xl mt-7 ml-2 font-extrabold pb-4 ">
             {' '}
-            {' { Existential Nihilism }'}
+            {'Existential Nihilism'}
           </span>
         </div>
       </Link>

11e8e0a0e056b44147c3aebe22d212c93bbddce5
Chiranjeev Thomas <chiranjeevwork@gmail.com>
2023-08-07 08:49:43 +0000

    7 Aug 2023, 14:19

--- components/BlogCardWithImage.tsx
+++ components/BlogCardWithImage.tsx
@@ -9,7 +9,7 @@ interface IPropType {
 const BlogCardWithImage = ({ article }: IPropType) => {
   console.log('blog with image slug', `/article/${article.attributes.Slug}`);
   return (
-    <div className="bg-gradient-to-r from-green-400 to-green-600 rounded-md flex items-center h-64">
+    <div className="bg-gradient-to-r from-green-400 to-green-600 rounded-md flex items-center h-64 justify-between">
       <Link href={`/article/${article.attributes.Slug}`}>
         <span
           className="text-2xl w-2/3 text-white p-6 after:content-[''] font-bold after:content-end after:bg-white after:block after:w-20 after:h-1 
@@ -20,7 +20,13 @@ const BlogCardWithImage = ({ article }: IPropType) => {
             `${article.attributes.Title.length > 45 ? '...' : ''}`}
         </span>
       </Link>
-      <Image alt="book" src="/thought.svg" width={140} height={140} />
+      <Image
+        alt="book"
+        src="/thought.svg"
+        width={140}
+        height={140}
+        className="px-2"
+      />
     </div>
   );
 };

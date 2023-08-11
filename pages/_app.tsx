be44d8e3d58f1914d96a6f05d7c0c8d971b9d373
Chiranjeev Thomas <chiranjeevwork@gmail.com>
2023-08-11 09:42:32 +0000

    11 Aug 2023, 15:12

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,23 @@
-import type { AppProps } from 'next/app';
-import { GoogleAnalytics } from 'nextjs-google-analytics';
+import type { AppProps, NextWebVitalsMetric } from 'next/app';
+import { GoogleAnalytics, event } from 'nextjs-google-analytics';
 import NextNProgress from 'nextjs-progressbar';
 import Footer from '../components/Footer';
 import NavBar from '../components/NavBar';
 import '../styles/globals.css';
+
+export function reportWebVitals({
+  id,
+  name,
+  label,
+  value,
+}: NextWebVitalsMetric) {
+  event(name, {
+    category: label === 'web-vital' ? 'Web Vitals' : 'Next.js custom metric',
+    value: Math.round(name === 'CLS' ? value * 1000 : value), // values must be integers
+    label: id, // id unique to current page load
+    nonInteraction: true, // avoids affecting bounce rate.
+  });
+}
 
 export default function App({ Component, pageProps }: AppProps) {
   return (

fbb84a234bc46fba0734b2e59878acc2118b675c
Chiranjeev Thomas <chiranjeevwork@gmail.com>
2023-08-11 09:20:50 +0000

    11 Aug 2023, 14:50

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,5 @@
 import type { AppProps } from 'next/app';
+import { GoogleAnalytics } from 'nextjs-google-analytics';
 import NextNProgress from 'nextjs-progressbar';
 import Footer from '../components/Footer';
 import NavBar from '../components/NavBar';
@@ -8,6 +9,7 @@ export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
       <div className="container mx-auto font-sans ml-5">
+        <GoogleAnalytics trackPageViews />
         <NextNProgress color="#53BD95" />
         <NavBar />
         <main className="pb-32">

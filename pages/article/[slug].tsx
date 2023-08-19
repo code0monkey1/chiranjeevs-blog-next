74bacbe00f6b8b158670ce582e4320b597220380
Chiranjeev Thomas <chiranjeevwork@gmail.com>
2023-08-19 05:43:55 +0000

    19 Aug 2023, 11:13

--- pages/article/[slug].tsx
+++ pages/article/[slug].tsx
@@ -17,6 +17,7 @@ import {
   RedditShareButton,
   TelegramIcon,
   TelegramShareButton,
+  TwitterIcon,
   TwitterShareButton,
   WhatsappIcon,
   WhatsappShareButton,
@@ -100,7 +101,7 @@ const Article = ({ article, notFound = false }: TPropTypes) => {
 
               <a className="ml-3 text-gray-500">
                 <FacebookShareButton url={shareUrl}>
-                  <FacebookIcon className="w-2" />
+                  <FacebookIcon className="w-5 h-5" />
                   {/* <svg
                     fill="currentColor"
                     strokeLinecap="round"
@@ -115,7 +116,8 @@ const Article = ({ article, notFound = false }: TPropTypes) => {
               </a>
               <a className="ml-3 text-gray-500">
                 <TwitterShareButton url={shareUrl}>
-                  <svg
+                  <TwitterIcon />
+                  {/* <svg
                     fill="currentColor"
                     strokeLinecap="round"
                     strokeLinejoin="round"
@@ -124,7 +126,7 @@ const Article = ({ article, notFound = false }: TPropTypes) => {
                     viewBox="0 0 24 24"
                   >
                     <path d="M23 3a10.9 10.9 0 01-3.14 1.53 4.48 4.48 0 00-7.86 3v1A10.66 10.66 0 013 4s-4 9 5 13a11.64 11.64 0 01-7 2c9 5 20 0 20-11.5a4.5 4.5 0 00-.08-.83A7.72 7.72 0 0023 3z"></path>
-                  </svg>
+                  </svg> */}
                 </TwitterShareButton>
               </a>
 

7469e0161b9cc70454f3af759f4a364596517c61
Chiranjeev Thomas <chiranjeevwork@gmail.com>
2023-12-14 09:25:26 +0000

    14 Dec 2023, 14:55

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,10 @@
+import { SpeedInsights } from "@vercel/speed-insights/next";
 import type { AppProps, NextWebVitalsMetric } from 'next/app';
 import { GoogleAnalytics, event } from 'nextjs-google-analytics';
 import NextNProgress from 'nextjs-progressbar';
 import Footer from '../components/Footer';
 import NavBar from '../components/NavBar';
 import '../styles/globals.css';
-
 export function reportWebVitals({
   id,
   name,
@@ -22,15 +22,19 @@ export function reportWebVitals({
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
+
       <div className="container mx-auto font-sans ml-5">
         <GoogleAnalytics trackPageViews={{ ignoreHashChange: true }} />
         <NextNProgress color="#53BD95" />
         <NavBar />
         <main className="pb-32">
-          <Component {...pageProps} />
+\
+             <Component {...pageProps} />
+\
         </main>
         <Footer />
       </div>
+
     </>
   );
 }
